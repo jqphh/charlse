48859c12d0a68ef6155cc4bc634cd90b9a0d9a23
charlse <jqphh@163.com>
2018-05-27 21:02:35 +0800

    爬虫抓取图片实现

--- http.py
+++ http.py
@@ -21,25 +21,11 @@ def http_post(host, port, uri, content):
         if client is not None:
             client.close()
 
+def http_get(url):
+    try:
 
-if __name__ == '__main__':
-    val = "{\"system\":{\"ipp\":\"3.0\",\"key\":\"key3.0\"},\"request\":{\"kid\":\"CH_SPEAKER_001\"}}"
-    ret = json.loads(val)
-    print "%s\n%s" % (ret['system'], ret['request'])
-    http_post("device.chiq-cloud.com", 8080, "/cdc/device/systime", "{}")
+    except Exception, e:
+        print e
+    finally:
 
-    data = {}
-    data['i'] = '星期天'
-    data['from'] = 'AUTO'
-    data['to'] = 'AUTO'
-    data['smartresult'] = 'dict'
-    data['client'] = 'fanyideskweb'
-    data['salt'] = '1515930176349'
-    data['sign'] = 'e93195638d3e86894d340ac2baa9b619'
-    data['doctype'] = 'json'
-    data['version'] = '2.1'
-    data['keyfrom'] = 'fanyi.web'
-    data['action'] = 'FY_BY_REALTIME'
-    data['typoResult'] = 'false'
-    param = urllib.urlencode(data)
-    http_post('fanyi.youdao.com', None, '/translate_o?smartresult=dict&smartresult=rule', param)
+
--- url.py
+++ url.py
@@ -2,6 +2,8 @@ import urllib
 import urllib2
 import cookielib
 import re
+import time
+import ftplib
 
 
 def my_http(url, param=None):
@@ -55,21 +57,62 @@ def my_http_with_cookie(url, param):
     except Exception, e:
         print e
 
+#my_http("http://passport.csdn.net/account/login", {'username': 'jqphh@163.com', 'password': '002318304'})
+
+
+def get_time_stamp():
+    ct = time.time()
+    local_time = time.localtime(ct)
+    data_head = time.strftime("%Y-%m-%d-%H-%M-%S", local_time)
+    data_secs = (ct - long(ct)) * 1000
+    time_stamp = "%s.%03d" % (data_head, data_secs)
+    return time_stamp
+
+
+def ftp_download(url, file_name):
+    if url is None or file_name is None:
+        return -1
+
+    cells = url.split('//')
+    cell = cells[1].split('/')
+    print cell[0]
+
+    try:
+        ftp = ftplib.FTP(host=cell[0])
+        fp = open(file_name, 'wb')
+        ftp.retrbinary('RETR ' + url, fp.write, 1024)
+        ftp.close()
+        ftp.quit()
+    except Exception, e:
+        print "[FTP Error]:%s" % e
+
+
+def download_img(url):
+    if url is None:
+        return -1
+
+    html_content = my_http(url)
+    if html_content is None:
+        return -2
+
+    reg = r'src="((http.:)?//.+\.(jpg|png|jpeg))"'
+    img_re = re.compile(reg)
+    img_list = re.findall(img_re, html_content)
+
+    for img_info in img_list:
+        #print img_info
+        if 'http' in img_info[1]:
+            img_url = img_info[0]
+        else:
+            img_url = 'https:' + img_info[0]
+
+        file_name = 'D:\Work\py\%s.%s' % (get_time_stamp(), img_info[2])
+        print img_url, file_name
+
+        # download img
+        urllib.urlretrieve(img_url, file_name)
+
 
 if __name__ == '__main__':
-    #my_http("http://passport.csdn.net/account/login", {'username': 'jqphh@163.com', 'password': '002318304'})
-    content = my_http('https://jx3.xoyo.com/index')
-    if content is not None:
-        try:
-            reg = r'src="(.+?\.jpg)"'
-            imgre = re.compile(reg)
-            imglist = re.findall(imgre, content)
-
-            x = 0
-
-            for imgurl in imglist:
-                print imgurl
-                #urllib.urlretrieve(imgurl, 'D:\Work\py\%s.jpg' % x)
-                x += 1
-        except Exception, e:
-            print e
+    download_img('https://n.163.com/#job')
+    download_img('https://jx3.xoyo.com/index')
